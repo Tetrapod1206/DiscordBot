5a658505049353da283f55c5de7615ee24277705
Tetrapod1206 <61351385+Tetrapod1206@users.noreply.github.com>
2021-02-22 21:16:41 +0800

    Update
    - command update
    - set : decide which channel is now tracing
    - init : refresh the history, only functional at the channel already be set
    - print : can call at any channel, print the history

--- bot.js
+++ bot.js
@@ -15,30 +15,42 @@ bot.on('ready',() => {
 bot.on("message", function (user, userID, channelID, message, evt){
     if(message.substring(0,1) == '!'){
         switch(message.substring(1)){
-            case("set"):
+            case("init"):
                 bot.getMessages({channelID:channelID,limit:100},function (err,messageArray){       
                     for (var msg of messageArray){
                         if(msg.content.substring(1) == "…"||msg.content.substring(1) == "..."){
                             hist = [msg.content.substring(0,1),hist].join('');
                         }
-                        console.log(msg.content.substring(1));
                     }
-                    bot.sendMessage({to: channelID, message:hist});
-                    solitarieChannel = channelID;
                 });
                 break;
 
+            case("set"):
+                solitarieChannel = channelID;
+                bot.sendMessage({to: channelID, message:"Channel set"});
+                break;
+            
+            case("print"):
+                bot.sendMessage({to: channelID, message:hist});
+                break;
+            /*    
+            This feature is abandon since there is a rate limit of deleting message by bot
             case("clear"):
                 if(channelID == solitarieChannel){
+                    console.log("clearing");
                     bot.getMessages({channelID:channelID,limit:100},function (err,messageArray){       
                         for (var msg of messageArray){
                             if(!(msg.content.substring(1) == "…"||msg.content.substring(1) == "...")){
-                                bot.deleteMessage(channelID,msg.id);
+                                setTimeout(function(){
+                                    bot.deleteMessage({channelID:channelID,messageID:msg.id},function (err){
+                                        console.log(err);
+                                    });
+                                },6000 );
                             }
                         }
                     });
                 }
-                break;
+                break;*/
 
             default :
                 bot.sendMessage({to: channelID, message:"Wrong channel"});
@@ -49,7 +61,6 @@ bot.on("message", function (user, userID, channelID, message, evt){
         if(channelID == solitarieChannel){
             var reply = message.substring(0,1);
             hist = [hist,reply].join("");
-            bot.sendMessage({to: channelID, message:hist});
         }
     }
 });

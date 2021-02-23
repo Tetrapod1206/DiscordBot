89449a3762a616f52a8c64ddf441020d5d6ea17f
Tetrapod1206 <61351385+Tetrapod1206@users.noreply.github.com>
2021-02-23 14:32:25 +0800

    add help command

--- bot.js
+++ bot.js
@@ -1,8 +1,10 @@
 var Discord = require('discord.io');
 var logger = require('winston');
 var auth = require('./auth.json');
+var fs = require('fs');
 var hist = "";
 var solitarieChannel;
+var utilityCaneel;
 
 var bot = new Discord.Client({
     token:auth.token,autorun:true
@@ -15,7 +17,15 @@ bot.on('ready',() => {
 bot.on("message", function (user, userID, channelID, message, evt){
     if(message.substring(0,1) == '!'){
         switch(message.substring(1)){
+            case("help"):
+                fs.readFile('help.txt', function(err,data){
+                    console.log(err);
+                    bot.sendMessage({to:channelID, message:data});
+                    console.log(data);
+                });
+                break;
             case("init"):
+                solitarieChannel = channelID;
                 bot.getMessages({channelID:channelID,limit:100},function (err,messageArray){       
                     for (var msg of messageArray){
                         if(msg.content.substring(1) == "…"||msg.content.substring(1) == "..."){
@@ -26,15 +36,14 @@ bot.on("message", function (user, userID, channelID, message, evt){
                 break;
 
             case("set"):
-                solitarieChannel = channelID;
-                bot.sendMessage({to: channelID, message:"Channel set"});
+                utilityChannel = channelID;
+            //    message.react(":ok:");
                 break;
             
             case("print"):
                 bot.sendMessage({to: channelID, message:hist});
                 break;
-            /*    
-            This feature is abandon since there is a rate limit of deleting message by bot*/
+
             case("clear"):
                 if(channelID == solitarieChannel){
                     console.log("clearing");
@@ -55,7 +64,7 @@ bot.on("message", function (user, userID, channelID, message, evt){
                 break;
 
             default :
-                bot.sendMessage({to: channelID, message:"Wrong channel"});
+            //    message.react(":negative_squared_cross_mark:");
         }
     }
 
